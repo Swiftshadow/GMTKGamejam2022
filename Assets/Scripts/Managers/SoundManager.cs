f6bf4055ec278b76808c53482a0859003cddeaf3
swiftshadow <swiftshadow34@gmail.com>
2022-07-16 15:23:16 -0500

    Adds a volume-changing function for the pause menu, and assigns some channels to the gamemanager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SoundManager
 {
+    public static float volume = 1;
+    
     public static void PlaySound(AudioClip clip)
     {
         GameObject soundGO = new GameObject("Sound");
         soundGO.AddComponent<DelayedDestroy>();
         AudioSource source = soundGO.AddComponent<AudioSource>();
-        source.PlayOneShot(clip);
+        source.PlayOneShot(clip, volume);
     }
 }
--- Assets/Scripts/UI/PauseBehaviour.cs
+++ Assets/Scripts/UI/PauseBehaviour.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PauseBehaviour : StateInteractor
 {
+
+    private AudioSource bgm;
+
+    private void Start()
+    {
+        bgm = GameObject.FindWithTag("GameAssets").GetComponent<AudioSource>();
+    }
+    
     public void Close()
     {
         requestStateChange.RaiseEvent((int)GameManager.GameState.Paused);
@@ -13,6 +21,12 @@ public class PauseBehaviour : StateInteractor
     {
         requestStateChange.RaiseEvent((int)GameManager.GameState.Menu);
         LoadManager.Instance.LoadSingle("Title");
+    }
+
+    public void SetVolume(float value)
+    {
+        bgm.volume = value * 0.2f;
+        SoundManager.volume = value;
     }
     
     protected override void OnStateChange(int arg0)
