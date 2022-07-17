e871d95ff29c60338b8146392f738d6a4404a562
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-07-17 10:50:25 -0500

    Stat menu confirm button

--- Assets/Scripts/UI/StatShop.cs
+++ Assets/Scripts/UI/StatShop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatShop : StateInteractor
 {
@@ -10,6 +11,8 @@ public class StatShop : StateInteractor
     [SerializeField] private IntChannel sellStatChannel;
 
     [SerializeField] private VoidChannel confirmStatChannel;
+
+    [SerializeField] private Button confirmButton;
 
     public void BuyStat(int index)
     {
@@ -29,6 +32,18 @@ public class StatShop : StateInteractor
             requestStateChange.RaiseEvent((int)GameManager.GameState.Transition);
             string[] temp = { "StatShop" };
             LoadManager.Instance.UnloadAddative(temp);
+        }
+    }
+
+    private void Update()
+    {
+        if (GameManager.Instance.GetStat(3) == 0 && confirmButton.interactable == false)
+        {
+            confirmButton.interactable = true;
+        }
+        else if(GameManager.Instance.GetStat(3) != 0 && confirmButton.interactable == true)
+        {
+            confirmButton.interactable = false;
         }
     }
 
